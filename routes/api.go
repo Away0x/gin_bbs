d2bf309c44332b2d634d22c2ac0855f9a0cd0a0b
Away0x <wutong0910@foxmail.com>
2019-05-14 17:27:32 +0800

    rate limit

--- routes/api.go
+++ routes/api.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"gin_bbs/pkg/ginutils/router"
+	"gin_bbs/routes/middleware"
+	"time"
 
 	"gin_bbs/app/controllers/api/user"
 	vericode "gin_bbs/app/controllers/api/verification_code"
@@ -14,10 +16,10 @@ func registerAPI(r *router.MyRoute, middlewares ...gin.HandlerFunc) {
 
 	// 短信验证码
 	r.Register("POST", "api.verificationCodes.store", "verificationCodes",
-		// middleware.RateLimiter(1*time.Minute, 1), // 1 分钟 1 次
+		middleware.RateLimiter(1*time.Minute, 10), // 1 分钟 10 次
 		vericode.Store)
 	// 用户注册
 	r.Register("POST", "api.users.store", "users",
-		// middleware.RateLimiter(1*time.Minute, 1), // 1 分钟 1 次
+		middleware.RateLimiter(1*time.Minute, 10), // 1 分钟 10 次
 		user.Store)
 }
